b4cb85d2ecd3a70825ee79c9226e146f0fd8e485
BG239176 <zjlin@800best.com>
2018-12-11 15:29:13 +0800

    更改文件名，符合python命名规范

--- generate_md_file.py
+++ generate_md_file.py
@@ -7,6 +7,7 @@ import time
 
 
 def read_file_name():
+    """获取用户输入的文件名"""
     try:
         return sys.argv[1] + '.md'
     except IndexError:
@@ -15,6 +16,7 @@ def read_file_name():
 
 
 def create_template():
+    """创建markdown文件模板"""
     return '---' + \
            '\nTitle: YourBlogTitle' + \
            '\nDate: ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + \
@@ -29,12 +31,14 @@ def create_template():
 
 
 def create_file(filename, template):
+    """创建文件，并写入模板"""
     file = open(filename, mode='w')
     file.write(template)
     file.close()
 
 
 def generate_md_file():
+    """自动生成markdown文件"""
     filename = read_file_name()
     if filename is not None:
         template = create_template()
